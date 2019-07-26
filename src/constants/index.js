80563e14bed12e66009d24d069bdc3477c23d71b
Joshua Kaluba <joshkaluba@gmail.com>
2019-07-25 20:56:24 -0400

    Created index file for constants

---
+++ src/constants/index.js
@@ -0,0 +1,13 @@
+import StringDictionary from './StringDictionary';
+import Colors from './Colors';
+import Layout from './Layout';
+import ApplicationDefaultSettings from './ApplicationDefaultSettings';
+import DefaultStyles from './DefaultStyles';
+
+export {
+    StringDictionary,
+    Colors,
+    Layout,
+    ApplicationDefaultSettings,
+    DefaultStyles
+}
