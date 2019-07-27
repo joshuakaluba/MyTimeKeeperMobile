e41c7d27abcb58fa5dcc3d14b7898fd51dab9294
Joshua Kaluba <joshkaluba@gmail.com>
2019-07-27 18:05:00 -0400

    Created application default form input

---
+++ src/components/PrimaryFormInput.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { TextInput } from 'react-native-paper';
+
+export default class PrimaryFormInput extends React.Component {
+
+    static propTypes = {
+        onChangeText: PropTypes.func.isRequired,
+        label: PropTypes.string.isRequired
+    };
+
+    render() {
+        return (
+            <TextInput
+                mode={'outlined'}
+                label={this.props.label}
+                onChangeText={this.props.onChangeText}
+            />
+        );
+    }
+}
