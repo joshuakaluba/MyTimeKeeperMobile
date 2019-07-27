1e379f53cb580548a199e8404cf2fa28f355d46f
Joshua Kaluba <joshkaluba@gmail.com>
2019-07-27 18:04:20 -0400

    Added theme and provider for react native paper UI framework

--- App.js
+++ App.js
@@ -3,10 +3,21 @@ import * as Font from 'expo-font';
 import React, { useState } from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import { Colors } from './src/constants';
 import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App(props) {
   const [isLoadingComplete, setLoadingComplete] = useState(false);
+
+  const theme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      primary: Colors.primary,
+      accent: 'yellow',
+    },
+  };
 
   if (!isLoadingComplete && !props.skipLoadingScreen) {
     return (
@@ -18,10 +29,12 @@ export default function App(props) {
     );
   } else {
     return (
-      <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-        <AppNavigator />
-      </View>
+      <PaperProvider theme={theme}>
+        <View style={styles.container}>
+          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+          <AppNavigator />
+        </View>
+      </PaperProvider>
     );
   }
 }
