3c17b7a0705df71f58ecba2d2f14fb4131a97f48
Joshua Kaluba <joshkaluba@gmail.com>
2019-07-29 20:31:15 -0400

    Updated string resources

--- src/constants/StringDictionary.js
+++ src/constants/StringDictionary.js
@@ -1,4 +1,18 @@
 export default StringDictionary = {
     myActivities: 'My Activities',
-    search: 'Search'
+    history: 'History',
+    newActivityNameValidationMessage: 'Please enter a valid activity name',
+    addNewActivity: 'Add New Activity',
+    activityDescription: 'Activity Description (Optional)',
+    activityName: 'Activity Name',
+    search: 'Search',
+    cancel: 'Cancel',
+    save: 'Save Changes',
+    ok: 'Ok',
+    startActivity: 'Start Activity',
+    stopActivity: 'Stop Activity',
+    edit: 'Edit',
+    delete: 'Delete',
+    deleteConfirmation: 'The following action will delete log history. Are you sure you want to delete activity: ',
+    inProgress: 'In Progress'
 }
