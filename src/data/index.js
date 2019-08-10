ec89df0ff666b87a62e9161f9b72402b483f2d95
Joshua Kaluba <joshkaluba@gmail.com>
2019-08-09 21:56:19 -0400

    Created index for repositories

---
+++ src/data/index.js
@@ -0,0 +1,9 @@
+import BaseRepository from './BaseRepository';
+import ActivityRepository from './ActivityRepository';
+import ActivityLogRepository from './ActivityLogRepository';
+
+export {
+    BaseRepository,
+    ActivityRepository,
+    ActivityLogRepository
+}
