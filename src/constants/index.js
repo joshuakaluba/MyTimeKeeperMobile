2f538f2db88621206bd49f932141b16b7277fb07
Joshua Kaluba <joshkaluba@gmail.com>
2019-08-19 20:27:26 -0400

    Added  device events

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,11 +3,13 @@ import Colors from './Colors';
 import Layout from './Layout';
 import ApplicationDefaultSettings from './ApplicationDefaultSettings';
 import DefaultStyles from './DefaultStyles';
+import DeviceEvents from './DeviceEvents';
 
 export {
     StringDictionary,
     Colors,
     Layout,
     ApplicationDefaultSettings,
-    DefaultStyles
+    DefaultStyles,
+    DeviceEvents
 }
