0c938364464a7a6adb8159b1647ff8d62f67bedf
Joshua Kaluba <joshkaluba@gmail.com>
2019-09-01 18:44:43 -0400

    Formatting changes

--- src/constants/ApplicationDefaultSettings.js
+++ src/constants/ApplicationDefaultSettings.js
@@ -1,18 +1,16 @@
-import Colors from './Colors';
+import Colors from "./Colors";
 
 export default ApplicationDefaultSettings = {
     tabBarOptions: {
         activeTintColor: Colors.tabActiveTintColor,
         inactiveTintColor: Colors.tabInactiveTintColor,
-        style: {
-            backgroundColor: Colors.tabBackgroundColor
-        }
+        style: { backgroundColor: Colors.tabBackgroundColor }
     },
     headerStyle: {
-        backgroundColor: Colors.headerBackgroundColor,
+        backgroundColor: Colors.headerBackgroundColor
     },
     headerTintColor: Colors.headerTintColor,
     headerTitleStyle: {
-        fontWeight: 'bold',
-    },
-}
+        fontWeight: "bold"
+    }
+};
